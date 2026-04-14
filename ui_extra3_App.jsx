f5d2461f8420ee8da3071e8facd28c53ce23bf8d
SuzyYiXu <54336649+SuzyYiXu@users.noreply.github.com>
2026-04-14 14:01:30 -0700

    Set up routing for the application with React Router

--- ui_extra3_App.jsx
+++ ui_extra3_App.jsx
@@ -1,5 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+// Import the components we designed
 import LoginPage from './LoginPage';
 import Dashboard from './Dashboard';
 import OperationsPortal from './OperationsPortal';
 import CommunityManagement from './CommunityManagement';
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Public Route */}
+        <Route path="/login" element={<LoginPage />} />
+        
+        {/* Private Routes (The pages that show after login) */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/operations" element={<OperationsPortal />} />
+        <Route path="/community" element={<CommunityManagement />} />
+
+        {/* Default redirect: send user to Login if they go to the root URL */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
